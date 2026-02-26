300dc7772b1ea97345342791246d3873d0f0f5ed
irakum <iringreen1@gmail.com>
2026-02-26 21:05:40 +0200

    main

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,81 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void main() {
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    IO.println(String.format("Hello and welcome!"));
+import java.util.ArrayList;
+import java.util.Random;
 
-    for (int i = 1; i <= 5; i++) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        IO.println("i = " + i);
+public class Main {
+    public static void main(String[] args) {
+        for (int N = 100; N < 3101; N+=200) {
+            task(3000, 10, N, 10);
+        }
+    }
+
+    public static void task(double simulationTime, int iterations, int N, int delay) {
+        long avgTimeSim1 = 0;
+        for (int oi = 1; oi < iterations; oi++) {
+            Element.resetNextId();
+            Create creator = new Create("CREATOR", delay);
+
+            Process prevProcessor = new Process("PROCESSOR0", delay);
+            creator.addNextElement(prevProcessor);
+
+            ArrayList<Process> listOfProcesses = new ArrayList<>();
+            listOfProcesses.add(prevProcessor);
+
+            for (int i = 1; i < N; i++) {
+                Process process = new Process("PROCESSOR" + i, delay);
+                listOfProcesses.add(process);
+                prevProcessor.addNextElement(process);
+
+                prevProcessor = process;
+            }
+            Model model = new Model(new ArrayList<>(){{ add(creator); addAll(listOfProcesses); }});
+            avgTimeSim1 += measureTime(() -> model.simulate(simulationTime));
+        }
+
+        long avgTimeSim2 = 0;
+        for (int oi = 1; oi < iterations; oi++) {
+            Element.resetNextId();
+            Create creator = new Create("CREATOR", delay);
+
+            Process prevProcessor = new Process("PROCESSOR0", delay);
+            creator.addNextElement(prevProcessor);
+
+            ArrayList<Process> listOfProcesses = new ArrayList<>();
+            listOfProcesses.add(prevProcessor);
+
+            for (int i = 1; i < N; i++) {
+                Process process = new Process("PROCESSOR" + i, delay);
+                listOfProcesses.add(process);
+            }
+
+            for (int i = 0; i < listOfProcesses.size()/2; i++) {
+
+                Process current = listOfProcesses.get(i);
+
+                int leftChild = 2 * i + 1;
+                int rightChild = 2 * i + 2;
+
+                if (leftChild < listOfProcesses.size()) {
+                    current.addNextElement(listOfProcesses.get(leftChild), 0.5);
+                }
+                if (rightChild < listOfProcesses.size()) {
+                    current.addNextElement(listOfProcesses.get(rightChild), 0.5);
+                }
+            }
+
+            Model model = new Model(new ArrayList<>(){{ add(creator); addAll(listOfProcesses); }});
+            avgTimeSim2 += measureTime(() -> model.simulate(simulationTime));
+        }
+
+        System.out.println("При N=" + N);
+        System.out.println("Час симуляції №1: " + (avgTimeSim1 /(double)iterations));
+        System.out.println("Час симуляції №2: " + (avgTimeSim2 /(double)iterations));
+        System.out.println("----------------------------------");
+    }
+
+    private static long measureTime(Runnable runnable) {
+        long startTime = System.currentTimeMillis();
+        runnable.run();
+        long endTime = System.currentTimeMillis();
+        return endTime - startTime;
     }
 }
